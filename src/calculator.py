e5eec069abf499b0adb4cf32876305bcbdfbe584
haitam <h.lahrichi05@gmail.com>
2026-03-18 15:32:00 +0000

    feat(calculator) : creation de la fonction division (#1)

--- src/calculator.py
+++ src/calculator.py
@@ -5,3 +5,9 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+def divide(a, b):
+    """Retourne la division de a par b. Lance une exception"""
+    """ si elle detecte une erreur."""
+    if b == 0:
+        raise ValueError("Division par zero est impossible")
+    return a / b
